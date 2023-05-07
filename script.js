9ca1c38abbf59017347b244fc114701b671a0f94
Harsh V Srivastava <harshvsri@gmail.com>
2023-05-07 15:23:18 +0530

    Add UI to rps

--- script.js
+++ script.js
@@ -1,41 +1,79 @@
-const choices = ["Rock", "Paper", "Scissor"];
+const result = document.querySelector(".result");
+const choicesMade = document.createElement("h2");
+const resultPara = document.createElement("h2");
+const score = document.createElement("h2");
+let userScore = 0;
+let computerScore = 0;
+let gameOver = false;
 
-const getComputerChoice = () => choices[Math.floor(Math.random() * 3)];
-
-const getUserChoice = () => {
-  let userChoice = prompt("Enter Rock/Paper/Scissor: ");
-  return userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
-};
-
-const errorCheck = (choices, userChoice) => {
-  if (choices.includes(userChoice)) {
-    return true;
+const isGameOver = () => {
+  if (userScore === 5 || computerScore === 5) {
+    const finalResult = document.createElement("h2");
+    finalResult.textContent = userScore === 5 ? "YOU WON" : "YOU LOST";
+    result.appendChild(finalResult);
+    result.removeChild(choicesMade);
+    result.removeChild(resultPara);
+    result.removeChild(score);
+    gameOver = true;
+    return;
   }
-  return false;
 };
 
-const logic = (userChoice, computerChoice) => {
+const getComputerChoice = () => {
+  const choices = ["Rock", "Paper", "Scissor"];
+  return choices[Math.floor(Math.random() * 3)];
+};
+
+const handleClick = (event) => {
+  if (gameOver) return;
+
+  const userChoice = event.target.innerText;
+  const computerChoice = getComputerChoice();
+  const res = gameLogic(userChoice, computerChoice);
+
+  choicesMade.textContent = `You Chose: ${userChoice} AND Computer Chose: ${computerChoice}`;
+  resultPara.textContent = res.result;
+  score.textContent = `Your Score: ${res.userScore} AND Computer Score: ${res.computerScore}`;
+};
+
+const gameLogic = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    console.log(`Its a tie!, ${userChoice} ${computerChoice}`);
+    userScore++;
+    computerScore++;
+    isGameOver();
+    return {
+      result: `Its a tie!, ${userChoice} ${computerChoice}`,
+      userScore: `${userScore}`,
+      computerScore: `${computerScore}`,
+    };
   } else if (
     (userChoice === "Rock" && computerChoice === "Scissor") ||
     (userChoice === "Paper" && computerChoice === "Rock") ||
     (userChoice === "Scissor" && computerChoice === "Paper")
   ) {
-    console.log(`You Won!, ${userChoice} beats ${computerChoice}`);
+    userScore++;
+    isGameOver();
+    return {
+      result: `You Won!, ${userChoice} beats ${computerChoice}`,
+      userScore: `${userScore}`,
+      computerScore: `${computerScore}`,
+    };
   } else {
-    console.log(`You Lose!, ${computerChoice} beats ${userChoice}`);
+    computerScore++;
+    isGameOver();
+    return {
+      result: `You Lose!, ${computerChoice} beats ${userChoice}`,
+      userScore: `${userScore}`,
+      computerScore: `${computerScore}`,
+    };
   }
 };
 
-const computerChoice = getComputerChoice();
-let errResult = false;
-do {
-  const userChoice = getUserChoice();
-  errResult = errorCheck(choices, userChoice);
+const userChoices = document.querySelectorAll(".option");
+userChoices.forEach((choice) => {
+  choice.addEventListener("click", handleClick);
+});
 
-  if (errResult) {
-    logic(userChoice, computerChoice);
-    break;
-  }
-} while (!errResult);
+result.appendChild(choicesMade);
+result.appendChild(resultPara);
+result.appendChild(score);
